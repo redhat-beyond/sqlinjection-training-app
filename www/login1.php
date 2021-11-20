0e88c2771934a185e1d0ce508008e573ba48ca9f
Noam180 <92365149+Noam180@users.noreply.github.com>
2021-11-20 13:15:45 +0200

    Update login1.php
    Add a validation for special chars

--- www/login1.php
+++ www/login1.php
@@ -58,11 +58,24 @@ ini_set('display_errors', 1);
         <div class="col-lg-6">
 
 <?php
+   function anti_SQL_injection($my_string){
+     $flag = preg_match('/[\'\/~`\!@#\$%\^&\*\(\)_\-\+=\{\}\[\]\|;:"\<\>,\.\?\\\]/', $my_string);
+      if($flag == 1)
+{
+         print("Username has not been accepted - invalid character. No SQL injection in our class;
+         exit();
+}
+   }
+?>
+
+
+<?php
 //echo md5("pa55w0rd");
 
 if (!empty($_REQUEST['uid'])) {
 $username = ($_REQUEST['uid']);
 $pass = $_REQUEST['password'];
+anti_SQL_injection($username);
 
 $q = "SELECT * FROM users where username='".$username."' AND password = '".md5($pass)."'" ;
 
